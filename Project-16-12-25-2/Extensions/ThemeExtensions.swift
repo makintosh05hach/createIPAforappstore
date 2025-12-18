e6eca20b3d4554ad46e71d1a335c05e5e66e6c87
makintosh05hach <ushijimali235898@icloud.com>
2025-12-18 01:18:46 -0800

    Delete Project-16-12-25-2 directory

--- Project-16-12-25-2/Extensions/ThemeExtensions.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-extension View {
-    func theme(_ themeManager: ThemeManager, colorScheme: ColorScheme) -> AppTheme {
-        themeManager.resolvedTheme(colorScheme: colorScheme)
-    }
-}
